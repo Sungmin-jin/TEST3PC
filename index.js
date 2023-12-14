81fe87bda8500278211e1107f26119006db90040
Sungmin-jin <jasonjin1998@gmail.com>
2023-12-14 12:34:21 -0500

    reset

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
-
-const PORT = process.env.PORT || 4500;
-app.listen(PORT);
